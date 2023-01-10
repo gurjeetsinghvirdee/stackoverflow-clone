feda038c7a55eaeaf690de54c3094cc22c723709
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-01-11 02:23:14 +0530

    navbar styling done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                 <Link to='/' className='nav-item nav-btn'>For Teams</Link>
                 <form>
                     <input type='text' placeholder='Search...' />
-                    <FiSearch />
+                    <FiSearch className='search-icon'/>
                 </form>
                 { User === null ?
                     <Link to='/Auth' className='nav-item nav-links'>Log in</Link> :
